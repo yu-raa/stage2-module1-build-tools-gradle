89ddf9b07343dace533ad8e2686a716db6d150cb
Yuliya Raikova <yuradev2001@gmail.com>
2022-12-29 10:43:24 +0300

    ?

--- demo/src/main/java/com/epam/demo/Utils.java
+++ demo/src/main/java/com/epam/demo/Utils.java
@@ -4,6 +4,17 @@ import java.util.List;
 
 public class Utils {
     public static boolean isAllPositiveNumbers(List<String> args) {
-        //magic happens here
+        for (String str : args)
+            try {
+                double doub = Double.parseDouble(str);
+                if (doub < 0) {
+                    return false;
+                }
+            }
+            catch (NumberFormatException | NullPointerException e) {
+                return false;
+            }
+
+        return true;
     }
 }
--- utils/src/main/java/com/epam/utils/StringUtils.java
+++ utils/src/main/java/com/epam/utils/StringUtils.java
@@ -2,6 +2,11 @@ package com.epam.utils;
 
 public class StringUtils {
     public static boolean isPositiveNumber(String str) {
-         //here magic will happen
+        try {
+            return Double.parseDouble(str) > 0;
+        }
+        catch (NumberFormatException | NullPointerException e) {
+            return false;
+        }
     }
 }
